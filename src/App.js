bc7ad687466af35669a0cf31fc99e8f97a90d0f9
Dave Kanter <moatgoat@hotmail.com>
2019-08-21 13:44:02 -0700

    Got it started and going.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Container, Row, Col } from 'react-bootstrap'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="background-wrapper object-fit_contain">
+        <Container className="content-wrapper">
+          <Row>
+            <Col>12 column layout</Col>
+          </Row>
+          <Row>
+            <Col md={8}>8</Col>
+            <Col md={4}>4</Col>
+          </Row>
+          <Row>
+            <Col md={6}>6</Col>
+            <Col md={6}>6</Col>
+          </Row>
+          <Row>
+            <Col md={3}>3</Col>
+            <Col md={3}>3</Col>
+            <Col md={3}>3</Col>
+            <Col md={3}>3</Col>
+          </Row>
+          <Row className="footer">
+          <Col md={1}>Footer</Col>
+            <Col>3</Col>
+            <Col>6</Col>
+            <Col>9</Col>
+            <Col>12</Col>
+          </Row>
+        </Container>
     </div>
   );
 }
